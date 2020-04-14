33ccd4ef73adc4e5f39fa78e13a905eac36e7180
Brandon Conway <brandoncc@gmail.com>
2020-04-14 16:31:11 -0700

    Add redux dev tools in dev environment

--- src/_store/index.js
+++ src/_store/index.js
@@ -1,19 +1,26 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
 import createSocketMiddleware from './sockets';
 import rootReducer from '../_reducers';
 
 const socketMiddleware = createSocketMiddleware();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
+const middleware = process.env.NODE_ENV === 'development'
+  ? composeWithDevTools(
+    applyMiddleware(
+      thunk,
+      socketMiddleware,
+      // logger,
+    )
+  )
+  : applyMiddleware(
     thunk,
     socketMiddleware,
     // logger,
-  ),
-);
+  )
+
+const store = createStore(rootReducer, middleware);
 
 export default store;
