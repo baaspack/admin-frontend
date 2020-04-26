cc2abf436aa10d10d7ab9c9966873f7a8e3a73c1
imidom <imidom@gmail.com>
2020-04-26 15:46:31 -0400

    Add SDK instructions to dashboard

--- src/components/BackpackDashboard/Header.js
+++ src/components/BackpackDashboard/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import styles from './styles.module.css';
 
 function Header () {
-  return <h1 className={styles.header}>my backpacks</h1>
+  return <Fragment>
+    <h1 className={styles.header}>my backpacks</h1>
+    <p>Find your SDK at: {`${window.location.origin}`}/sdk.js. <a href="https://github.com/baaspack/baas-sdk/blob/master/README.md" target="_blank">Learn how to use it!</a></p>
+  </Fragment>
 }
 
 export default Header;
